4982b4470e378cd4111e495daa9155b2a106ec95
usoock13 <usoock13@gmail.com>
2023-04-13 02:13:29 +0900

    Movement component 1.1.1 is running as looks good to me.

--- Project SSD/Assets/Scripts/Entity/Movement.cs
+++ Project SSD/Assets/Scripts/Entity/Movement.cs
@@ -10,11 +10,15 @@ public class Movement : MonoBehaviour {
     [SerializeField] private float height = 1f;
     [SerializeField] private float radius = .5f;
     [SerializeField] private float distanceFromBottom = .4f;
+    [SerializeField] private float pullingDistance = .4f;
+    [SerializeField] private bool useGravity;
+
+    private float fallingSpeed = 0f;
     private CapsuleBounds colliderBounds {
         get {
             if(ownCollider == null) return new CapsuleBounds();
-            return new CapsuleBounds(transform.position - Vector3.up*(height*.5f - distanceFromBottom - radius),
-                                     transform.position + Vector3.up*(height*.5f + distanceFromBottom + radius),
+            return new CapsuleBounds(transform.position + Vector3.up*(distanceFromBottom + radius),
+                                     transform.position + Vector3.up*(distanceFromBottom + radius + height),
                                      radius);
         }
     }
@@ -24,12 +28,14 @@ public class Movement : MonoBehaviour {
     // [System.NonSerialized] public bool isPullingdown = true;
     #endregion Collider Information
 
-    protected virtual void Awake() {}
-    protected virtual void Start() {
+    protected virtual void Awake() {
         if(!TryGetComponent<CapsuleCollider>(out ownCollider)) {
             Debug.LogError("Movement Component should be with 'CapsuleCollider' in same GameObject.");
+        } else {
+            ownCollider.radius = radius;
         }
     }
+    protected virtual void Start() {}
     
     public void MoveToward(Vector3 direction) {
         (Vector3 point1, Vector3 point2, float radius) = colliderBounds.RaycastComponent;
@@ -41,38 +47,47 @@ public class Movement : MonoBehaviour {
         Debug.DrawLine(point1, point2+ direction + Vector3.down*radius, Color.green);
         Debug.DrawLine(point1+ direction, point2 + Vector3.down*radius, Color.green);
         hits = Physics.CapsuleCastAll(point1, point2, radius, direction, direction.magnitude, blockLayer);
-        print(direction.normalized);
-        foreach(RaycastHit hit in hits) {
-            // print(hit.distance);
-        }
+        Vector3 normal = direction.normalized;
         if(hits.Length > 0) {
-            Vector3 rdir = Vector3.zero;
             foreach(RaycastHit hit in hits) {
-                // print(hit.transform.name);
-                print(hit.normal);
-                rdir += hit.normal;
+                float xx = direction.normalized.x + (Mathf.Abs(direction.normalized.x)>Mathf.Abs(hit.normal.x) ? hit.normal.x : -direction.normalized.x);
+                float yy = direction.normalized.y + (Mathf.Abs(direction.normalized.y)>Mathf.Abs(hit.normal.y) ? hit.normal.y : -direction.normalized.y);
+                float zz = direction.normalized.z + (Mathf.Abs(direction.normalized.z)>Mathf.Abs(hit.normal.z) ? hit.normal.z : -direction.normalized.z);
+                normal = new Vector3(xx, yy, zz);
             }
-            rdir = new Vector3(Mathf.Clamp(rdir.x, -1, 1), Mathf.Clamp(rdir.y, -1, 1), Mathf.Clamp(rdir.z, -1, 1));
-            transform.Translate(direction.magnitude * (direction.normalized + rdir).normalized, Space.World);
+            Debug.DrawLine(transform.position + normal, transform.position + normal*5, Color.red);
+            // print(normal);
+            // rdir = new Vector3(Mathf.Clamp(rdir.x, -1, 1), Mathf.Clamp(rdir.y, -1, 1), Mathf.Clamp(rdir.z, -1, 1));
+            transform.Translate(direction.magnitude * normal, Space.World);
         } else {
             transform.Translate(direction, Space.World);
         }
-
-        Pulldown();
     }
     private void Pulldown() {
         (Vector3 point1, Vector3 point2, float radius) = colliderBounds.RaycastComponent;
         RaycastHit hit;
-        bool isGround = Physics.CapsuleCast(point1, point2, radius, Vector3.down, out hit, distanceFromBottom, blockLayer);
-        float gap = (hit.distance);
-        float pullingForce = -gap * pullingCoef;
-        if(isGround && gap>.01f) {
+        bool isGround = Physics.CapsuleCast(point1, point2, radius, Vector3.down, out hit, distanceFromBottom+pullingDistance, blockLayer);
+        float gap = distanceFromBottom - hit.distance;
+        
+        float pullingForce = gap * pullingCoef;
+        if(isGround) {
+            fallingSpeed = 0;
             transform.Translate(new Vector3(0, pullingForce, 0));
+        } else {
+            fallingSpeed += Physics.gravity.y * Time.deltaTime;
         }
+        if(useGravity)
+            transform.Translate(0, fallingSpeed * Time.deltaTime, 0);
+    }
+    void Update() {
+        Pulldown();
     }
     void OnDrawGizmos() {
         Gizmos.color = Color.red;
         if(ownCollider != null)
-            Gizmos.DrawWireCube(colliderBounds.center, new Vector3(radius*2, height+radius, radius*2));
+            Gizmos.DrawWireCube(colliderBounds.center, new Vector3(radius*2, height+radius*2, radius*2));
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(colliderBounds.center + Vector3.down*(height*.5f+radius) + Vector3.down*distanceFromBottom, new Vector3(1, 0, 1));
     }
 }
