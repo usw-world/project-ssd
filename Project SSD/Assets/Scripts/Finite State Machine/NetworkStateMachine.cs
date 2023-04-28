0270da95d75e20f38f4ad893bf1ac057c2fff8b7
usoock13 <usoock13@gmail.com>
2023-04-28 18:13:52 +0900

    Fix t he StateMachin's critial bug.

--- Project SSD/Assets/Scripts/Finite State Machine/NetworkStateMachine.cs
+++ Project SSD/Assets/Scripts/Finite State Machine/NetworkStateMachine.cs
@@ -1,49 +1,5 @@
 using UnityEngine;
 
 public class NetworkStateMachine : StateMachine {
-    public State currentState { get; private set; }
-    State nextState;
-
-    [HideInInspector] public bool isMuted = false;
-
-    bool hasBeenChanged = false;
-
-    private void Start() {
-        if(currentState == null) {
-            Debug.LogError($"StateMachine on '{gameObject.name}' has not any 'currentState'. Please set initial 'currentState' with SetInitialState(State state).");
-        }
-    }
-    public void SetIntialState(State state) {
-        if(currentState == null) {
-            currentState = state;
-        } else {
-            Debug.LogError($"StateMachine on '{gameObject.name}' already has initial 'currentState'.");
-        }
-    }
-    public void ChangeState(State nextState, bool intoSelf=true) {
-        if (hasBeenChanged || isMuted) return;
-        if(!intoSelf && nextState==currentState) return;
-        if(currentState == null) {
-            Debug.LogError($"StateMachine on '{gameObject.name}' has not any 'currentState'. Please set initial 'currentState' with SetInitialState(State state).");
-        } else {
-            currentState?.onInactive?.Invoke(nextState);
-            nextState?.onActive?.Invoke(currentState);
-            this.nextState = nextState;
-            currentState = nextState;
-            hasBeenChanged = true;
-        }
-    }
-    private void LateUpdate() => hasBeenChanged = false;
-    public bool Compare(State target) {
-        return currentState == target;
-    }
-    public bool Compare(string tag) {
-        return currentState.stateTag == tag;
-    }
-    private void Update() {
-        currentState?.onStay?.Invoke();
-    }
-    private void FixedUpdate() {
-        currentState?.onStayFixed?.Invoke();
-    }
+    
 }
--- Project SSD/Assets/Scripts/Finite State Machine/StateMachine.cs
+++ Project SSD/Assets/Scripts/Finite State Machine/StateMachine.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class StateMachine : MonoBehaviour {
     public State currentState { get; private set; }
-    State nextState;
+    protected State nextState;
 
     [HideInInspector] public bool isMuted = false;
 
-    bool hasBeenChanged = false;
+    protected bool hasBeenChanged = false;
 
-    private void Start() {
+    protected virtual void Start() {
         if(currentState == null) {
             Debug.LogError($"StateMachine on '{gameObject.name}' has not any 'currentState'. Please set initial 'currentState' with SetInitialState(State state).");
         }
@@ -17,7 +17,7 @@ public class StateMachine : MonoBehaviour {
         if(currentState == null) {
             currentState = state;
         } else {
-            Debug.LogError($"StateMachine on '{gameObject.name}' already has initial 'currentState'.");
+            Debug.LogError($"StateMachine on '{gameObject.name}' already has initial 'currentState' as {currentState}.");
         }
     }
     public void ChangeState(State nextState, bool intoSelf=true) {
@@ -33,17 +33,17 @@ public class StateMachine : MonoBehaviour {
             hasBeenChanged = true;
         }
     }
-    private void LateUpdate() => hasBeenChanged = false;
+    protected virtual void LateUpdate() => hasBeenChanged = false;
     public bool Compare(State target) {
         return currentState == target;
     }
     public bool Compare(string tag) {
         return currentState.stateTag == tag;
     }
-    private void Update() {
+    protected virtual void Update() {
         currentState?.onStay?.Invoke();
     }
-    private void FixedUpdate() {
+    protected virtual void FixedUpdate() {
         currentState?.onStayFixed?.Invoke();
     }
 }
