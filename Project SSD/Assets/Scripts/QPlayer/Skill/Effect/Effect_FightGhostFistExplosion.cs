28e32e233a09844a482f9b22680aefc7e7eda046
Hyunso Oh <dhgustn218@gmail.com>
2023-06-05 20:12:56 +0900

    " QP 스킬 거이 다댐 필살기 만드는중"

--- Project SSD/Assets/Scripts/QPlayer/Skill/Effect/Effect_FightGhostFistExplosion.cs
+++ Project SSD/Assets/Scripts/QPlayer/Skill/Effect/Effect_FightGhostFistExplosion.cs
@@ -6,7 +6,18 @@ public class Effect_FightGhostFistExplosion : MonoBehaviour, IPoolableObject
 {
 	public void Run(float damageAmount)
 	{
-		// 오버렙으로 데미지 주기
+		Collider[] hit = Physics.OverlapSphere(transform.position, 2f, 1 << 8);
+
+		for (int i = 0; i < hit.Length; i++)
+		{
+			Damage damage = new Damage(
+				damageAmount,
+				0,
+				Vector3.zero,
+				Damage.DamageType.Normal
+			) ;
+			hit[i].GetComponent<IDamageable>().OnDamage(damage);
+		}
 	}
 	public string GetKey()
 	{
