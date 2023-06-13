5ed6bb7d1e2586e9150b35d7e02c6b84aa2072cd
usoock13 <usw@g.yju.ac.kr>
2023-06-13 20:53:17 +0900

    The Pteranodon is comming...

---
+++ Project SSD/Assets/Prefabs/Effects/Enemy/Chrome DIno/Effect_PteranodonAssault.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect_PteranodonAssault : MonoBehaviour {
+	[SerializeField] private float amount = 15f;
+	[SerializeField] private float forceScalar = 25f;
+    public bool isActive = false;
+	private bool hasHit = false;
+    
+    public void Active() {
+        isActive = true;
+        hasHit = false;
+    }
+    public void Inactive() {
+        isActive = false;
+    }
+	private void OnTriggerEnter(Collider other) {
+        if(!hasHit
+        && isActive
+		&& other.gameObject.layer == 7) {
+			Vector3 force = (other.transform.position - transform.position).normalized * forceScalar;
+			Damage damage = new Damage(
+				amount,
+				.55f,
+				force,
+				Damage.DamageType.Normal
+			); 
+			other.GetComponent<IDamageable>()?.OnDamage(damage);
+			hasHit = true;
+        }
+    }
+}
